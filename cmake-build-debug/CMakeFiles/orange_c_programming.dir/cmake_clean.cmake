db29556ff60aeb36033a97c3df49031315b7d448
javamon1174 <javamon1174@gmail.com>
2018-12-24 06:34:53 +0900

    first commit

---
+++ cmake-build-debug/CMakeFiles/orange_c_programming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orange_c_programming.dir/main.c.o"
+  "orange_c_programming.pdb"
+  "orange_c_programming"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/orange_c_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
